d0f7ae3cf172ff6beb860095c115905f83e92579
Mansvini Aggarwal <coderspecial@gmail.com>
2023-04-01 20:33:21 +0530

    adding err console

--- controllers/image.js
+++ controllers/image.js
@@ -7,7 +7,10 @@ const app = new Clarifai.App({
 export const handleApiCall=(req, res)=>{
 	app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
 		.then(data=>res.json(data))
-		.catch(err=>res.status(400).json('unable to work with API'))
+		.catch(err=>{
+			console.log(err);
+			res.status(400).json('unable to work with API')
+		})
 }
 
 
